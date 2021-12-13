0b0967abe95e00d9487176e6bdc870ff6d055fc6
Florian Caron <caron.florian7620@gmail.com>
2021-12-13 17:00:03 +0100

    add files

--- Module 1/Maximo.java
+++ Module 1/Maximo.java
@@ -1,53 +1,114 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Maximo {
+
+	public static final String CHEMIN_FICHIER = "Module 1/Ressources/dictionnaire.txt";
+	public static final int NB_MOTS = 22506;
+	public static Random r = new Random();
+
 	public static void main(String[] args) {
+		try {
+			// Tirage du mot aléatoirement
+			String mot = tirerMotAleatoirement();
+			System.out.println("DEBUG: Mot = " + mot);
 
-		String nbPremiers = "";
-		int limite = saisirInt("Veuillez saisir la limite de recherche :");
+			// Mélange du mot
+			char[] motMelange = melanger(mot);
 
-		if (limite >= 2) {
-			nbPremiers = "2";
+			// Saisie de l'utilisateur 
+
+
+			// Vérification de la saisie de l'utilisateur
+			boolean correct = false;
+			while (!correct){
+				char[] saisie = saisieUtilisateur(motMelange);
+				correct = bonnesLettres(saisie, mot);
+				correct = dansLeDico(saisie);
+			}
+			
+		} catch (IOException e) {
+			System.err.println("ERREUR : " + e );
 		}
 
-		for (int nbr = 3; nbr <= limite; nbr += 2) {
+	}
 
-			int nbDiviseurs = 0;
-			int compteur = 3;
+	private static boolean bonnesLettres(char[] saisie, char[] mot) {
+		System.out.println("DEBUG: -- Vérification des lettres --");
+		for (int i = 0; i < saisie.length; i++) {
+			for (int j = 0; j < mot.length; j++) {
 
-			while ((nbDiviseurs <= 1) && (compteur <= nbr)) {
-				if ((nbr % compteur) == 0) {
-					nbDiviseurs++;
+			}
+		}
+	}
+
+	private static boolean dansLeDico(char[] saisie){
+		System.out.println("DEBUG: -- Recherche d'une correspondance --");
+
+		try (FileInputStream fichier = new FileInputStream(CHEMIN_FICHIER);
+			Scanner s = new Scanner(fichier)) {
+				int numLigne = r.nextInt(NB_MOTS);
+				for (int i = 0; i < numLigne-1; i++) {
+					s.nextLine();
 				}
-				compteur += 2;
+				return s.nextLine();
 			}
+	}
 
-			if (nbDiviseurs == 1) {
-				nbPremiers += ", " + nbr;
+	private static char[] saisieUtilisateur(char[] motMelange){
+		System.out.println("DEBUG: -- Saisie de l'utilisateur --");
+
+		try {
+			System.out.println("Pouvez-vous trouver le mot mélangé ? (minuscules)");
+			System.out.println(String.valueOf(motMelange));
+			Scanner s = new Scanner(System.in); 
+			String saisie = s.nextLine();
+			s.close();
+			return saisie.toCharArray();
+		} catch (Exception e) {
+			System.err.println("ERREUR : " + e );
+		}
+		return null;
+	}
+
+	private static char[] melanger(String mot) {
+		System.out.println("DEBUG: -- Mélange du mot. --");
+		try {
+			char[] motTableau = mot.toLowerCase().toCharArray();
+			char[] motMelange = motTableau;
+			int aleatoire = 0;
+			char temporaire = 'a';
+			// Mélange du mot
+			for (int i = 0; i < motTableau.length; i++) {
+				aleatoire = r.nextInt(motTableau.length);
+				temporaire = motMelange[i];
+				motMelange[i] = motTableau[aleatoire];
+				motTableau[aleatoire] = temporaire;
 			}
+			return motMelange;
+		} catch (Exception e) {
+			System.out.println("ERREUR : " + e);
 		}
-
-		if (nbPremiers.isEmpty()) {
-			nbPremiers = "Aucun r�sultat";
-		}
-
-		System.out.println("Les nombres premiers de 0 � " + limite + " sont :");
-		System.out.println(nbPremiers);
-
+		return null;
 	}
 
 	/**
-	 * 
-	 * Affiche un message et retourne une saisie de type Int
-	 * 
-	 * @param message
-	 *            � afficher
-	 * @return La saisie entr�e par l'utilisateur
+	 * Retourne un mot tiré aléatoirement
+	 * @return 
+	 * @throws IOException
 	 */
-	private static int saisirInt(String message) {
-		System.out.println(message);
-		Scanner scan = new Scanner(System.in);
-		return scan.nextInt();
+	private static String tirerMotAleatoirement() throws IOException {
+		System.out.println("DEBUG: -- Tirage du mot aléatoire. --");
+		try (FileInputStream fichier = new FileInputStream(CHEMIN_FICHIER);
+			Scanner s = new Scanner(fichier)) {
+				int numLigne = r.nextInt(NB_MOTS);
+				System.out.println("DEBUG: Numéro de ligne = " + numLigne);
+				for (int i = 0; i < numLigne-1; i++) {
+					s.nextLine();
+				}
+				return s.nextLine();
+			}
 	}
-
 }
